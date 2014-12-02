59136a7ab540b29eafa94487c5bfc9c83ae623a4
David Vale <davidvale@MBPdeDavidVale.home>
2014-12-02 03:19:37 +0000

    header

---
+++ assets/js/global.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".wrap_menu_button").click(function(){
+    $(".header_nav").toggleClass("nav_active");
+  });
+  $(".wrap_menu_button").click(function(){
+    $(".menu_button").toggleClass("show");
+  });
+});
