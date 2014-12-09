941a48d026d33a4333874efff8f96a9023bf0d9b
David Vale <davidvale@MBPdeDavidVale.home>
2014-12-09 02:02:21 +0000

    work page

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,13 +1,16 @@
 
-/* Global Nav */
+ // Global Nav 
 $(document).ready(function(){
   $(".wrap_menu_button").click(function(){
   	$(".header").toggleClass("header_on");
     $(".header_nav").toggleClass("nav_active");
     $(".menu_button").toggleClass("on");
   });
+
+  // archive section - work page
   $(".load_more_btn").click(function(){
   	$(".work__row__archive").removeClass("archived");
+  	$(".work__cell").removeClass("archived");
   	$(".load_more").addClass("no_btn");
   });
 });
