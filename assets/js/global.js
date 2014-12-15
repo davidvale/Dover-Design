c29597efd9cd305e2e1fca47192beb963b3fc6b0
David Vale <davidvale@MBPdeDavidVale.home>
2014-12-15 02:40:28 +0000

    work pages

--- assets/js/global.js
+++ assets/js/global.js
@@ -8,9 +8,56 @@ $(document).ready(function(){
   });
 
   // archive section - work page
-  $(".load_more_btn").click(function(){
-  	$(".work__row__archive").removeClass("archived");
-  	$(".work__cell").removeClass("archived");
-  	$(".load_more").addClass("no_btn");
+  $(".archive_btn").click(function(){
+  	$(".work__row.more_proj").toggleClass("archived");
+    $(".archive_btn.more").toggleClass("no_btn");
+    $(".archive_btn.less").toggleClass("no_btn");
   });
+
+  // work pop-up
+  $(".close_btn").click(function(){
+    $(".pop-up_wraper").addClass("no_pop-up");
+    $(".wrapper").removeClass("no_scroll");
+  });
+  $(".case-study_btn").click(function(){
+    $(".pop-up_wraper").removeClass("no_pop-up");
+    $(".wrapper").addClass("no_scroll");
+  });
+
+  // Remove rollover
+  if ('ontouchstart' in document.documentElement) {
+    try {
+      var ignore = /:hover/;
+
+      function removeRules(sheet) {
+        for (var j = sheet.cssRules.length - 1; j >= 0; j--) {
+          var rule = sheet.cssRules[j];
+          if (rule.type === CSSRule.MEDIA_RULE) {
+            removeRules(rule);
+          }
+          else if (rule.type === CSSRule.STYLE_RULE) {
+            if(ignore.test(rule.selectorText)) {
+              sheet.deleteRule(j);
+            }
+          }
+        }
+      }
+
+      for (var i = 0; i < document.styleSheets.length; i++) {
+        var sheet = document.styleSheets[i];
+        if (!sheet.cssRules) {
+          continue;
+        }
+        
+        removeRules(sheet);
+      }
+    } catch (e) {}
+  }
+
+
+
+
 });
+
+
+
